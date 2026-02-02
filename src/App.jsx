0eacdf860d95a8dafed75faf29d9ec75b8c9ad80
anmolsah <kumarxxx242424@gmail.com>
2026-02-02 19:38:40 +0530

    feat: Add initial landing page component and configure application routing.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './context/LanguageContext';
 import Layout from './components/Layout';
+import LandingPage from './pages/LandingPage';
 import Dashboard from './pages/Dashboard';
 import AskQuestion from './pages/AskQuestion';
 import QuestionDetails from './pages/QuestionDetails';
@@ -10,21 +11,17 @@ function App() {
   return (
     <LanguageProvider>
       <BrowserRouter>
-        <AppContent />
+        <Routes>
+          {/* Landing page - no layout wrapper */}
+          <Route path="/" element={<LandingPage />} />
+          
+          {/* App pages with sidebar layout */}
+          <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+          <Route path="/ask" element={<Layout><AskQuestion /></Layout>} />
+          <Route path="/question/:id" element={<Layout><QuestionDetails /></Layout>} />
+        </Routes>
       </BrowserRouter>
     </LanguageProvider>
-  );
-}
-
-function AppContent() {
-  return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/ask" element={<AskQuestion />} />
-        <Route path="/question/:id" element={<QuestionDetails />} />
-      </Routes>
-    </Layout>
   );
 }
 
